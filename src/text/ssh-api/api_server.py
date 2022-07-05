db1fc30b63a99a9b2cbf1df48ec9056920c0bd64
Toshiki Muromachi <ageki104@gmail.com>
2022-07-05 12:32:33 +0900

    fix

--- src/text/ssh-api/api_server.py
+++ src/text/ssh-api/api_server.py
@@ -3,7 +3,8 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 app.config['JSON_AS_ASCII'] = False    # json 文字コード修正
- 
+
+
 @app.route('/generate', methods=['GET'])
 def generate():
     req = request.args
@@ -11,6 +12,7 @@ def generate():
     print(type(text))
     text = 'Hello world' + text
     return jsonify({'message': text})
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8000, debug=True)
